32d807d0d0d285e194059e76202048f536dff883
marcinwasowicz <marcinkwasowicz@gmail.com>
2022-07-17 18:28:38 +0200

    implement unit test for dict_ops module

--- src/word_count_lib.rs
+++ src/word_count_lib.rs
@@ -1,2 +1,25 @@
 pub mod dict_ops;
 pub mod file_ops;
+
+#[cfg(test)]
+mod dict_ops_tests {
+    use super::*;
+    use std::collections::HashMap;
+
+    #[test]
+    fn test_dictionary_building() {
+        let elements = ["word1", "word2", "word1", "word3"]
+            .iter()
+            .map(|x| Ok(String::from(x.to_owned())));
+
+        let expected_dictionary: HashMap<String, i32> = HashMap::from_iter(
+            [("word1", 2), ("word2", 1), ("word3", 1)]
+                .iter()
+                .map(|(k, v)| (String::from(k.to_owned()), v.to_owned())),
+        );
+        assert_eq!(
+            dict_ops::build_dictionary(elements).unwrap(),
+            expected_dictionary
+        );
+    }
+}
